33e4789e4861d101e1ef10fd141c7f1cf0b574b0
CAFUCH <tjfdk6201@gmail.com>
2023-01-21 01:34:56 +0900

    20230120 - PM
    UI setting

--- Zoo/Assets/Scripts/Shop.cs
+++ Zoo/Assets/Scripts/Shop.cs
@@ -16,16 +16,18 @@ public class Shop : MonoBehaviour
         clicker = GetComponent<Clicker>();
     }
 
-    public void AnimalShop() {
+    public void AnimalShop() { //동물 구매
 
         curButton = EventSystem.current.currentSelectedGameObject; //누른 버튼의 이름을 받아온다
         
-        for (int i = 0; i < animalList.Count; i++) {
+        for (int i = 0; i < animalList.Count; i++) { //동물리스트 길이 만큼 반복
 
             if (animalList[i].name == curButton.name && animalPriceList[i] <= clicker.curGold) {
+                //누른 버튼과 동물의 이름이 같으며 필요한 돈보다 현재 돈이 많다면
 
-                clicker.curGold -= animalPriceList[i];
-                animalList[i].SetActive(true);
+                clicker.curGold -= animalPriceList[i]; //돈을 차감한다
+                if (Input.GetKeyDown(KeyCode.E))
+                    animalList[i].SetActive(true); //동물을 켜준다
             }
         }
     }
