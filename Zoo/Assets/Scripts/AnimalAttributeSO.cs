e720d2992e811d12ce210e1699650beda522088e
CAFUCH <tjfdk6201@gmail.com>
2023-01-23 15:38:18 +0900

    20230123 - PM
    UI setting

--- Zoo/Assets/Scripts/AnimalAttributeSO.cs
+++ Zoo/Assets/Scripts/AnimalAttributeSO.cs
@@ -6,11 +6,14 @@ using UnityEngine.UI;
 [CreateAssetMenu(fileName = "animalAttribute", menuName = "SO/animalAttribute", order = 0)]
 public class AnimalAttributeSO : ScriptableObject
 {
-    public string areaName;
-    public string animalName;
-    public int priceValue;
-    public Sprite gImage;
-    public Sprite animal1;
-    public Sprite animal2;
-    public Sprite animal3;
+    public string areaName; //지역 이름
+    public string animalName; //동물 이름
+    public Sprite gImage; //구역 사진
+    public int priceGValue; //구역 가격
+    public Sprite animal1; //동물 1 사진
+    public int priceA1Value; //동물 1 가격
+    public Sprite animal2; //동물 2 사진
+    public int priceA2Value; //동물 2 가격
+    public Sprite animal3; //동물 3 사진
+    public int priceA3Value; //동물 3 가격
 }
